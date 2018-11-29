8ddb71196774a196164cffcdabfd34dadebc1135
zhaoxianglinXL <15736535817@163.com>
2018-11-29 17:53:09 +0800

    Eureka Demo基本完成

--- src/main/java/com/winner/springcloud/DeptProvider8001_App.java
+++ src/main/java/com/winner/springcloud/DeptProvider8001_App.java
@@ -2,12 +2,15 @@ package com.winner.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author ZXL
  * @描述 主启动类
  */
 @SpringBootApplication
+//声明自己是注册中心的客户端
+@EnableEurekaClient //本服务启动后悔自动注册进eureka注册中心中
 public class DeptProvider8001_App {
 
 	public static void main(String[] args) {
