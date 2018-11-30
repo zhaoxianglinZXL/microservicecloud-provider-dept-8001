decc2e317c1ae3daeaa8c872e3d72e439eddea61
zhaoxianglinXL <15736535817@163.com>
2018-11-30 12:07:10 +0800

    服务的发现测试成功 注册中心集群配置成功

--- src/main/java/com/winner/springcloud/DeptProvider8001_App.java
+++ src/main/java/com/winner/springcloud/DeptProvider8001_App.java
@@ -2,6 +2,7 @@ package com.winner.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -11,6 +12,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 //声明自己是注册中心的客户端
 @EnableEurekaClient //本服务启动后悔自动注册进eureka注册中心中
+//声明自己是服务发现客户端
+@EnableDiscoveryClient //Enable***Client 申明自己是干什么的 客户端
 public class DeptProvider8001_App {
 
 	public static void main(String[] args) {
--- src/main/java/com/winner/springcloud/controller/DeptInfoController.java
+++ src/main/java/com/winner/springcloud/controller/DeptInfoController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +27,9 @@ public class DeptInfoController {
 	@Autowired
 	@Qualifier("deptInfoService")
 	private IDeptInfoService ideptInfoService;
+	
+	@Autowired
+	private DiscoveryClient client;//暴露服务
 	
 	//添加
 	@RequestMapping(value = "/addDept",method = RequestMethod.POST)
@@ -51,5 +56,27 @@ public class DeptInfoController {
 	public List<DeptInfo> queryAll(){
 		System.out.println("进入了");
 		return ideptInfoService.queryAll();
+	}
+	
+	/**
+	 * @作者：ZXL
+	 * @时间：2018年11月30日 上午10:01:30
+	 * @parameter
+	 * @return
+	 * @描述：循环出所有的微服务
+	 */
+	@RequestMapping(value="/discovery",method = RequestMethod.GET)
+	@ResponseBody
+	public Object discovery(){
+		List<String> list = client.getServices();
+		System.out.println("************"+list);
+		
+		List<ServiceInstance> serverList = client.getInstances("MICROSERVICECLOUD-DEPT");
+		for(ServiceInstance element : serverList){
+			// 主机名  -- 地址  -- 端口
+			System.out.println(element.getServiceId() +"\t"+ element.getHost() +"\t"+ element.getPort() 
+					+"\t"+ element.getUri() );
+		}
+		return this.client;
 	}
 }
